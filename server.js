73984677f4cfcf4428741c1c0a2759e06ab5cd37
Connor Wong <s10197876@connect.np.edu.sg>
2021-06-06 00:06:30 +0800

    test

--- server.js
+++ server.js
@@ -137,6 +137,12 @@ app.post("/drive/email", async (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // Listen
