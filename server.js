0ae96f3adeb7f6988253cf987fac306f8c711c2e
Connor Wong <s10197876@connect.np.edu.sg>
2021-06-06 01:01:36 +0800

    test

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 // Variables
 var sheetId = "";
@@ -135,14 +136,9 @@ app.post("/drive/email", async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  // Express serve up index.html file if it doesn't recognize route
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // Listen
 app.listen(PORT, () => {
