342ff3f653cf7b1e950f150aa5f31c13571a5e3b
Connor Wong <s10197876@connect.np.edu.sg>
2021-06-10 00:13:08 +0800

    make it better

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -34,7 +34,7 @@ export function register(config) {
     window.addEventListener("load", () => {
       const swFileName =
         process.env.NODE_ENV === "production"
-          ? "serviceWorker.js"
+          ? "service-worker.js"
           : "custom-sw.js";
       const swUrl = `${process.env.PUBLIC_URL}/${swFileName}`;
 
