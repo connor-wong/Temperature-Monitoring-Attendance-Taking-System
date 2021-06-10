55456062bf420210f184a7deb947b50c3eab1e0d
Connor Wong <s10197876@connect.np.edu.sg>
2021-06-10 12:19:57 +0800

    make it better

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "client", "build")));
 
 webpush.setVapidDetails(
