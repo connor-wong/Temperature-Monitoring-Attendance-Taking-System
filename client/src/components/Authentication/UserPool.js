378bd10f281765bd8fcdfd16743425e60484497b
Wong Zheng Hui <41357084+connor-wong@users.noreply.github.com>
2021-09-18 21:39:55 +0800

    Delete UserPool.js

--- client/src/components/Authentication/UserPool.js
+++
@@ -1,8 +0,0 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
-
-const poolData = {
-  UserPoolId: "ap-southeast-1_LW7VL5TAI",
-  ClientId: "n4hbcid1kla9n4mktonhu7fmk",
-};
-
-export default new CognitoUserPool(poolData);
